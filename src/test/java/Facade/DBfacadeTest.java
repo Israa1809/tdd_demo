c2ff31f52f0584016b3f432ffbfa6637ebde4163
Israa1809 <din-israa@hotmail.com>
2023-02-01 15:27:56 +0100

    updated test 1

--- src/test/java/Facade/DBfacadeTest.java
+++ src/test/java/Facade/DBfacadeTest.java
@@ -52,7 +52,7 @@ public class DBfacadeTest {
 
     @Test
     public void test() throws SQLException {
-        System.out.println("Testing database connection");
+        System.out.println("Testing database connection, to see first name");
         String sql = "SELECT fname from startcode_test.usertable";
         try (ResultSet set = con.prepareStatement(sql).executeQuery()) {
             set.next();
